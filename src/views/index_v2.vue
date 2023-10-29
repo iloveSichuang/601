59b6adfe3c7de8c682545b595674aaea33bdd4bb
haidong chen <1024130664@qq.com>
2023-10-29 23:13:35 +0800

    1029start

---
+++ src/views/index_v2.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>1</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  created(){
+    console.log(`output->index`)
+  }
+}
+</script>
+
+<style>
+
+</style>
